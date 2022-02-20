53e754ac9148225aa54737cd30f9af62d0044a05
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2022-02-20 01:31:20 -0500

    Add climb commands

---
+++ 2022Robot/src/main/java/frc/robot/commands/ExtendElevator.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimbSubsystem;
+
+public class ExtendElevator extends CommandBase {
+    private final ClimbSubsystem m_climbSubsystem;
+
+    public ExtendElevator(ClimbSubsystem climbSubsystem) {
+        m_climbSubsystem = climbSubsystem;
+        addRequirements(climbSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        m_climbSubsystem.setElevator(1.0);
+    }
+
+    @Override
+    public void initialize() {
+        m_climbSubsystem.stopElevator();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_climbSubsystem.stopElevator();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ 2022Robot/src/main/java/frc/robot/commands/TraverseRungs.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimbSubsystem;
+
+public class TraverseRungs extends CommandBase {
+    public TraverseRungs(ClimbSubsystem climbSubsystem) {
+        // to be programmed later
+    }
+}
