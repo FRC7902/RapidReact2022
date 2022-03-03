1064e4b57057778aa08f0133fec5e5d50b0b523c
MasterSeafood <yellowwatermelon14@gmail.com>
2022-03-03 12:53:05 -0500

    New set to height command and display statuses

---
+++ 2022Robot/src/main/java/frc/robot/commands/SetElevatorToHeight.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimbSubsystem;
+
+public class SetElevatorToHeight extends CommandBase {
+  private ClimbSubsystem m_climbSubsystem;
+  private double setHeight;
+  /** Creates a new SetElevatorToHeight. */
+  public SetElevatorToHeight(double setHeight, ClimbSubsystem climbSubsystem) {
+    m_climbSubsystem = climbSubsystem;
+    this.setHeight = setHeight;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_climbSubsystem);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_climbSubsystem.stopElevator();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(m_climbSubsystem.getElevatorRotations() > setHeight){
+      m_climbSubsystem.setElevator(-0.5);
+    }else{
+      m_climbSubsystem.setElevator(0.5);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_climbSubsystem.stopElevator();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return setHeight + 0.5 >= m_climbSubsystem.getElevatorRotations() && setHeight-0.5 <= m_climbSubsystem.getElevatorRotations();
+  }
+}
